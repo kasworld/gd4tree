05eeafa6d1f82d9115fcaaee657a34037ecfb61d
SeukWon Kang <kasworld@gmail.com>
2024-04-10 10:10:39 +0900

    update bartree

--- bar_tree/bar_tree.gd
+++ bar_tree/bar_tree.gd
@@ -9,25 +9,19 @@ var bar_rotation_base_velocity :float = 5.0
 
 var bar_height :float = tree_height/bar_count
 var bar_list :Array
+var auto_rotate_bar :float
 
-func set_params(w:float, h :float, bar_w :float, b_count:int, rot_vel :float)->void:
+func init_with_color(co1 :Color, co2:Color, emission :bool,
+		w: float, h :float, bar_w :float, b_count:int, rot_vel :float, autorot :float = 1.0/60.0)->void:
 	tree_height = h
 	tree_width = w
 	bar_width_base_rate = bar_w
-	bar_rotation_base_velocity = rot_vel
 	bar_count = b_count
 	bar_height = tree_height/bar_count
+
+	bar_rotation_base_velocity = rot_vel
+	auto_rotate_bar = autorot
 	clear_bar_list()
-
-func clear_bar_list()->void:
-	for sp in bar_list:
-		remove_child(sp)
-	bar_list.clear()
-
-func set_rotate_direction(dir :float)->void:
-	bar_rotation_base_velocity = dir
-
-func init_with_color(co1 :Color, co2:Color, emission :bool)->void:
 	for i in bar_count:
 		var rate = float(i)/bar_count
 		var bar_color = co1.lerp(co2,rate)
@@ -35,15 +29,34 @@ func init_with_color(co1 :Color, co2:Color, emission :bool)->void:
 		if emission:
 			mat.emission_enabled = true
 			mat.emission = bar_color
-		var sp = new_mat_inst3d(i,mat)
+		var sp = new_bar_inst3d(i,mat)
 		bar_list.append(sp)
 		add_child(sp)
 
-func init_with_material(mat :Material)->void:
+func init_with_material(mat :Material,
+		w: float, h :float, bar_w :float, b_count:int, rot_vel :float, autorot :float = 1.0/60.0)->void:
+	tree_height = h
+	tree_width = w
+	bar_width_base_rate = bar_w
+	bar_count = b_count
+	bar_height = tree_height/bar_count
+
+	bar_rotation_base_velocity = rot_vel
+	auto_rotate_bar = autorot
+	clear_bar_list()
 	for i in bar_count:
-		var sp = new_mat_inst3d(i,mat)
+		var sp = new_bar_inst3d(i,mat)
 		bar_list.append(sp)
 		add_child(sp)
+
+func clear_bar_list()->void:
+	for sp in bar_list:
+		remove_child(sp)
+	bar_list.clear()
+
+func _process(delta: float) -> void:
+	if auto_rotate_bar != 0.0:
+		bar_rotate_y(auto_rotate_bar)
 
 func get_color_mat(co: Color)->Material:
 	var mat = StandardMaterial3D.new()
@@ -52,7 +65,7 @@ func get_color_mat(co: Color)->Material:
 	mat.clearcoat = true
 	return mat
 
-func new_mat_inst3d(i :int, mat :Material)->MeshInstance3D:
+func new_bar_inst3d(i :int, mat :Material)->MeshInstance3D:
 	var pos_rev = float(bar_count-i)
 	var pos_rev_rate = pos_rev / float(bar_count)
 	var y = float(i) * bar_height
@@ -71,7 +84,7 @@ func reset_bar_rotation()->void:
 	for sp in bar_list:
 		sp.rotation = Vector3.ZERO
 
-func set_bar_rotate_y(delta:float)->void:
+func bar_rotation_y(delta:float)->void:
 	for i in bar_list.size():
 		bar_list[i].rotation.y =  delta * (bar_rotation_base_velocity * float(i)*PI/bar_count)
 
--- main.gd
+++ main.gd
@@ -5,49 +5,38 @@ var brown_img = preload("res://image/Dark-brown-fine-wood-texture.jpg")
 var floor_img = preload("res://image/floorwood.jpg")
 var leaf_img = preload("res://image/leaf.png")
 
-var tree_list :Array
-
 func _ready() -> void:
 	$DirectionalLight3D.look_at(Vector3.ZERO)
 
-	var tr :BarTree
-	tr = new_tree_at(Vector3(0,0,0))
-	tr.set_params(1,1, 10,200, 10.0)
-	tr.init_with_color(Color.BLACK, Color.YELLOW, true)
+	var t :BarTree
+	t = new_tree_at(Vector3(0,0,0))
+	t.init_with_color(Color.BLACK, Color.YELLOW, true,1,1, 10,200, 10.0,1.0/60.0)
 
-	tr = new_tree_at(Vector3(1.5,0,1.5))
-	tr.set_params(2,1.5, 3,20, 3.0)
-	tr.init_with_color(Color.RED, Color.BLUE,true)
+	t = new_tree_at(Vector3(1.5,0,1.5))
+	t.init_with_color(Color.RED, Color.BLUE,true,2,1.5, 3,20, 3.0,1.0/60.0)
 
-	tr = new_tree_at(Vector3(-1.5,0,-1.5))
-	tr.set_params(2,2, 3,200, 6.0)
-	tr.init_with_color(Color.BLUE, Color.RED,false)
+	t = new_tree_at(Vector3(-1.5,0,-1.5))
+	t.init_with_color(Color.BLUE, Color.RED,false,2,2, 3,200, 6.0,1.0/60.0)
 
 	var mat = StandardMaterial3D.new()
 	mat.albedo_texture = floor_img
 	#mat.uv1_triplanar = true
-	tr = new_tree_at(Vector3(1.5,0,-1.5))
-	tr.set_params(3,1.6, 3,100, -3.0)
-	tr.init_with_material(mat)
+	t = new_tree_at(Vector3(1.5,0,-1.5))
+	t.init_with_material(mat,3,1.6, 3,100, -3.0,1.0/60.0)
 
 	mat = StandardMaterial3D.new()
 	mat.albedo_texture = leaf_img
 	mat.uv1_triplanar = true
-	tr = new_tree_at(Vector3(-1.5,0,1.5))
-	tr.set_params(1.5,1.3, 3,70, -6.0)
-	tr.init_with_material(mat)
+	t = new_tree_at(Vector3(-1.5,0,1.5))
+	t.init_with_material(mat,1.5,1.3, 3,70, -6.0,1.0/60.0)
 
 func new_tree_at(p :Vector3)->BarTree:
-	var tr = tree_scene.instantiate()
-	add_child(tr)
-	tree_list.append(tr)
-	tr.position = p
-	return tr
+	var t = tree_scene.instantiate()
+	add_child(t)
+	t.position = p
+	return t
 
 func _process(delta: float) -> void:
 	var t = Time.get_unix_time_from_system() /10
 	$Camera3D.position = Vector3(sin(t)*2.5 ,2.5, cos(t)*2.5  )
 	$Camera3D.look_at(Vector3.ZERO)
-
-	for n in tree_list:
-		n.bar_rotate_y(delta)
