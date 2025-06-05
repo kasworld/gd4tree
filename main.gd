e0c95b1e7e00a697bd9cd43cae82bfdcf4cb6da8
Kang Seukwon <kasworld@gmail.com>
2025-06-05 11:46:54 +0900

    make alltype tree

--- main.gd
+++ main.gd
@@ -9,14 +9,14 @@ var leaf_img = preload("res://image/leaf.png")
 func _ready() -> void:
 	$DirectionalLight3D.look_at(Vector3.ZERO)
 
-	make_tree(Vector3(0,0,0),2,2)
-	make_tree(Vector3(1.5,0,1.5),2,2)
-	make_tree(Vector3(-1.5,0,-1.5),2,2)
-	make_tree(Vector3(1.5,0,-1.5),2,2)
-	make_tree(Vector3(-1.5,0,1.5),2,2)
+	var n := 4
+	var rd := 2*PI/n
+	var r := 2.0
+	for i in n:
+		var pos = Vector3(sin(rd*i)*r, 0 , cos(rd*i)*r)
+		make_tree(i, pos, 2,2)
 
-
-func make_tree(p :Vector3, wmax:float, hmax:float)->BarTree2:
+func make_tree(btt:int, p :Vector3, wmax:float, hmax:float)->BarTree2:
 	var t = tree2_scene.instantiate()
 	add_child(t)
 	t.position = p
@@ -24,7 +24,7 @@ func make_tree(p :Vector3, wmax:float, hmax:float)->BarTree2:
 	var h = randf_range(hmax*0.5,hmax*1.0)
 	var bw = w * randf_range(0.5 , 2.0)/10
 	var bc = randf_range(5,20)
-	match randi_range(0,3):
+	match btt:
 		0:
 			var mat = StandardMaterial3D.new()
 			mat.albedo_texture = floor_img
@@ -45,3 +45,18 @@ func _process(delta: float) -> void:
 	var t = Time.get_unix_time_from_system() /10
 	$Camera3D.position = Vector3(sin(t)*2.5 ,2.5, cos(t)*2.5  )
 	$Camera3D.look_at(Vector3.ZERO)
+
+var key2fn = {
+	KEY_ESCAPE:_on_button_esc_pressed,
+}
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed:
+		var fn = key2fn.get(event.keycode)
+		if fn != null:
+			fn.call()
+	elif event is InputEventMouseButton and event.is_pressed():
+		pass
+
+func _on_button_esc_pressed() -> void:
+	get_tree().quit()
