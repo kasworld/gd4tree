8fbd0d546a3b7c07342d59ef67df483210a70a67
seukwonkang <kasworld@gmail.com>
2025-12-13 18:59:49 +0900

    use MultiMeshShape

--- bar_tree_2/bar_tree_2.gd
+++ bar_tree_2/bar_tree_2.gd
@@ -38,36 +38,21 @@ func init_with_color(co1 :Color, co2:Color) -> BarTree2:
 	color_from = co1
 	color_to = co2
 
-	var mat := StandardMaterial3D.new()
-	mat.albedo_color = Color.WHITE
-	mat.vertex_color_use_as_albedo = true
-	init_mesh_multi1(mat)
-	$MultiMeshInstance3D.multimesh.use_colors = true # before set instance_count
-	# Then resize (otherwise, changing the format is not allowed).
-	init_mesh_multi2()
+	$MultiMeshShape.init_with_color(BoxMesh.new(), Color.WHITE, bar_count, Vector3.ZERO)
 	update_bar_transform()
 	update_bar_color()
 	return self
 
 func init_with_material(mat :Material) -> BarTree2:
-	init_mesh_multi1(mat)
-	# Then resize (otherwise, changing the format is not allowed).
-	init_mesh_multi2()
+	$MultiMeshShape.init_with_material(BoxMesh.new(), mat, bar_count, Vector3.ZERO)
 	update_bar_transform()
 	return self
-
-func init_mesh_multi1(mat :Material) -> void:
-	$MultiMeshInstance3D.multimesh.mesh.material = mat
-
-func init_mesh_multi2() -> void:
-	$MultiMeshInstance3D.multimesh.instance_count = bar_count
-	$MultiMeshInstance3D.multimesh.visible_instance_count = bar_count
 
 # also reset bar rotation
 func update_bar_transform() -> void:
 	# Set the transform of the instances.
 	var bar_height := tree_height/bar_count
-	for i in $MultiMeshInstance3D.multimesh.visible_instance_count:
+	for i in $MultiMeshShape.multimesh.visible_instance_count:
 		var rate := float(i)/bar_count
 		var rev_rate := 1 - rate
 		var bar_position := Vector3(0, i *bar_height +bar_height/2, tree_width * rev_rate /2 * bar_shift_rate)
@@ -77,19 +62,19 @@ func update_bar_transform() -> void:
 		var t := Transform3D(Basis(), bar_position)
 		t = t.rotated(Vector3(0,1,0), bar_rot)
 		t = t.scaled_local( bar_size )
-		$MultiMeshInstance3D.multimesh.set_instance_transform(i,t )
+		$MultiMeshShape.multimesh.set_instance_transform(i,t )
 
 func update_bar_color() -> void:
 	assert(use_color)
-	for i in $MultiMeshInstance3D.multimesh.visible_instance_count:
+	for i in $MultiMeshShape.multimesh.visible_instance_count:
 		var rate = float(i)/bar_count
-		$MultiMeshInstance3D.multimesh.set_instance_color(i,color_from.lerp(color_to,rate))
+		$MultiMeshShape.multimesh.set_instance_color(i,color_from.lerp(color_to,rate))
 
 func set_visible_bar_count(bar_count_a :int) -> void:
 	assert(bar_count_a >= 0)
 	bar_count = bar_count_a
-	assert($MultiMeshInstance3D.multimesh.instance_count >= bar_count)
-	$MultiMeshInstance3D.multimesh.visible_instance_count = bar_count
+	assert($MultiMeshShape.multimesh.instance_count >= bar_count)
+	$MultiMeshShape.multimesh.visible_instance_count = bar_count
 	update_bar_transform()
 	if use_color:
 		update_bar_color()
@@ -106,9 +91,9 @@ func _process(_delta: float) -> void:
 		bar_rotation_y()
 
 func bar_rotation_y() -> void:
-	for i in $MultiMeshInstance3D.multimesh.visible_instance_count:
-		var t :Transform3D = $MultiMeshInstance3D.multimesh.get_instance_transform(i)
+	for i in $MultiMeshShape.multimesh.visible_instance_count:
+		var t :Transform3D = $MultiMeshShape.multimesh.get_instance_transform(i)
 		var rate := float(i)/bar_count
 		var bar_rot := rate * bar_rotation
 		t = t.rotated(Vector3(0,1,0), bar_rot)
-		$MultiMeshInstance3D.multimesh.set_instance_transform(i,t )
+		$MultiMeshShape.multimesh.set_instance_transform(i,t )
